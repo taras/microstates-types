063fc1be6ba26ee22154d1383e401fb0c4466505
Mike Harris <mharris717@gmail.com>
2018-10-10 10:54:46 -0400

    CS

--- microstates/index.d.ts
+++ microstates/index.d.ts
@@ -6,33 +6,52 @@
 
 export as namespace Microstates;
 
-export interface Microstate<T,V> {
-  initialize(): T
-  initialize(v: V): T
+export interface FieldType<T, V> {
+  initialize(): T;
+  initialize(v: V): T;
 
-  set(): T
-  set(value: V): T
+  set(): T;
+  set(value: V): T;
 }
 
-export interface NumberType extends Microstate<NumberType, Number> {
-  increment(): NumberType
-  increment(step: Number): NumberType
+export interface NumberType extends FieldType<NumberType, Number> {
+  increment(): NumberType;
+  increment(step: Number): NumberType;
 
-  decrement(): NumberType
-  decrement(step: Number): NumberType
+  decrement(): NumberType;
+  decrement(step: Number): NumberType;
 }
 
-export interface StringType extends Microstate<StringType, String> {
-  concat(): StringType
-  concat(str: String): StringType
+export interface StringType extends FieldType<StringType, String> {
+  concat(): StringType;
+  concat(str: String): StringType;
 }
 
-export function create<Number>(Type: typeof Number, value?: Number): NumberType
-export function create<String>(Type: typeof String, value?: String): StringType
-export function create<T,V>(Type: new(...args: any[]) => T, value?: V): MappedToRoot<T, T>
+export interface Root {}
 
-export type MappedToRoot<R,T> = {
-  [M in keyof T]: T[M] extends NumberType ? T[M] : R
-}
- 
+export function create<Number>(Type: typeof Number, value?: Number): NumberType;
+export function create<String>(Type: typeof String, value?: String): StringType;
+export function create<T, V>(
+  Type: new (...args: any[]) => T,
+  value?: V
+): MappedToRoot<T>;
 
+export type AllReturnRoot<R, T> = {
+  [M in keyof T]: T[M] extends Function ? (...args: any[]) => R : R
+};
+
+// export type MappedToRoot<R, T> = {
+//   [M in keyof T]: T[M] extends String
+//     ? AllReturnRoot<R, T[M]>
+//     : MappedToRoot<R, R>
+// };
+
+// export type MappedToRoot<R, T> = {
+//   [M in keyof T]: T[M] extends Function ? () => R : AllReturnRoot<R, T[M]>
+// };
+
+export type MappedToRoot<R> = {
+  [M in keyof R]: R[M] extends (NumberType | StringType)
+    ? AllReturnRoot<MappedToRoot<R>, R[M]>
+    : (R[M] extends Function ? (...args: any[]) => MappedToRoot<R> : R[M])
+};
